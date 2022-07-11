2a63ea0e02dd21bf591fa001f10305acdd11c065
Nikolayilin13 <salence1309@gmail.com>
2022-07-11 13:51:09 +0900

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikolay Ilyin");
+        System.out.println("13.09.1987");
+
+    }
+
+}
