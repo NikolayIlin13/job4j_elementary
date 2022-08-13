0d6fd6d520fd9188a97ded28fceea59181a025d4
Nikolayilin13 <salence1309@gmail.com>
2022-08-13 17:41:13 +0900

    4.3.1. Сокращенный оператор && [#333783]

--- src/test/java/ru/job4j/condition/DummyBotTest.java
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -1,16 +1,16 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
-import org.testng.Assert;
+import static org.assertj.core.api.Assertions.*;
 
-public class DummyBotTest {
+class DummyBotTest {
 
     @Test
-   public void whenGreetBot() {
+    public void whenGreetBot() {
         String in = "Привет, бот.";
         String result = DummyBot.answer(in);
         String expected = "Привет, умник.";
-        Assert.assertEquals(result, expected);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
@@ -18,7 +18,7 @@ public class DummyBotTest {
         String in = "Пока.";
         String result = DummyBot.answer(in);
         String expected = "До скорой встречи.";
-        Assert.assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
@@ -26,6 +26,6 @@ public class DummyBotTest {
         String in = "сколько будет 2+ 2?";
         String result = DummyBot.answer(in);
         String expected = "Это ставит меня в тупик. Задайте другой вопрос.";
-        Assert.assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
     }
 }
